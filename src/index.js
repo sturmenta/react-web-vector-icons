cf885fd398f7f8ae6868bf9fbfc427fddf2992c7
Amir <jopp@Jopps-MacBook-Pro.local>
2019-06-18 11:12:26 +0430

    export create Icon so developrs can create custom icon set

--- src/index.js
+++ src/index.js
@@ -13,37 +13,64 @@ export const Octicons = createIcon(require('./glyphmaps/Octicons.json'), 'Octico
 export const SimpleLineIcons = createIcon(require('./glyphmaps/SimpleLineIcons.json'), 'SimpleLineIcons');
 export const Zocial = createIcon(require('./glyphmaps/Zocial.json'), 'Zocial');
 
-// ────────────────────────────────────────────────────────────────────────────────
+
+export const createCustomSet = createIcon;
 
 import React, { Component } from 'react';
 
-class Icon extends Component{
-  render(){
-    let Icon = null;
-    switch (this.props.font) {
-      case 'AntDesign': Icon = AntDesign; break;
-      case 'Entypo': Icon = Entypo; break;
-      case 'EvilIcons': Icon = EvilIcons; break;
-      case 'Feather': Icon = Feather; break;
-      case 'FontAwesome': Icon = FontAwesome; break;
-      case 'Foundation': Icon = Foundation; break;
-      case 'Ionicons': Icon = Ionicons; break;
-      case 'MaterialIcons': Icon = MaterialIcons; break;
-      case 'MaterialCommunityIcons': Icon = MaterialCommunityIcons; break;
-      case 'Octicons': Icon = Octicons; break;
-      case 'Zocial': Icon = Zocial; break;
-      case 'SimpleLineIcons': Icon = SimpleLineIcons; break;
-      default: Icon = Ionicons; break;
+class Icon extends Component {
+    render() {
+        let Icon = null;
+        switch (this.props.font) {
+            case 'AntDesign':
+                Icon = AntDesign;
+                break;
+            case 'Entypo':
+                Icon = Entypo;
+                break;
+            case 'EvilIcons':
+                Icon = EvilIcons;
+                break;
+            case 'Feather':
+                Icon = Feather;
+                break;
+            case 'FontAwesome':
+                Icon = FontAwesome;
+                break;
+            case 'Foundation':
+                Icon = Foundation;
+                break;
+            case 'Ionicons':
+                Icon = Ionicons;
+                break;
+            case 'MaterialIcons':
+                Icon = MaterialIcons;
+                break;
+            case 'MaterialCommunityIcons':
+                Icon = MaterialCommunityIcons;
+                break;
+            case 'Octicons':
+                Icon = Octicons;
+                break;
+            case 'Zocial':
+                Icon = Zocial;
+                break;
+            case 'SimpleLineIcons':
+                Icon = SimpleLineIcons;
+                break;
+            default:
+                Icon = Ionicons;
+                break;
+        }
+        return (
+            <Icon
+                name={this.props.name}
+                color={this.props.color ? this.props.color : 'grey'}
+                size={this.props.size ? this.props.size : 30}
+                style={this.props.style}
+            />
+        );
     }
-    return(
-      <Icon
-        name={this.props.name}
-        color={this.props.color ? this.props.color : 'grey'}
-        size={this.props.size ? this.props.size : 30}
-        style={this.props.style}
-      />
-    );
-  }
 }
 
 export default Icon;
